3a201d8ba1c484ef3cf71acd2617b928dbf60f43
tafbilgiislem <tafbilgiislem@gmail.com>
2026-04-10 15:09:16 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const GITHUB_CONFIG = {
+    token: 'ghp_BURAYA_SENIN_TOKENIN_GELECEK', // GitHub'dan aldığın token
+    owner: 'tafbilgiislem',           // Örn: 'ahmet_yilmaz'
+    repo: 'ypm',                // Örn: 'sunum-deposu'
+    path: 'slayt.svg',                        // SVG dosyasının adı ve yolu
+    branch: 'main'                            // Genelde 'main' veya 'master' olur
+};
+
+export default GITHUB_CONFIG;
